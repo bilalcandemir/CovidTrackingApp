71c28ca0d1cc9a1d7420b81ab24fae0ed65c85f8
Bilal Candemir <candemirbilal@yahoo.com>
2021-02-14 15:11:34 +0300

    Initial Commit

---
+++ CovidTrackingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidTrackingApp
+//
+//  Created by Bilal Candemir on 2/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
