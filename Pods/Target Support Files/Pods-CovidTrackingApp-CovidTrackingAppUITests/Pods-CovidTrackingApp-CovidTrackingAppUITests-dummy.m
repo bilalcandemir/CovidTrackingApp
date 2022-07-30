0589e8bb7d3616a829c915869df129910fd34a50
Bilal Candemir <candemirbilal@yahoo.com>
2022-07-30 19:17:42 +0300

    First Commit

---
+++ Pods/Target Support Files/Pods-CovidTrackingApp-CovidTrackingAppUITests/Pods-CovidTrackingApp-CovidTrackingAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CovidTrackingApp_CovidTrackingAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CovidTrackingApp_CovidTrackingAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-CovidTrackingApp/Pods-CovidTrackingApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CovidTrackingApp : NSObject
+@end
+@implementation PodsDummy_Pods_CovidTrackingApp
+@end
